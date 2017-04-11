861994ae5e72848922574711890b99e321388be1
Eptick <redzicleon@gmail.com>
2017-04-11 18:19:30 +0200

    dev

--- application/libraries/Wesly.php
+++ application/libraries/Wesly.php
@@ -16,24 +16,21 @@ class Wesly {
 
             $url = "https://graph.facebook.com/v2.6/me/messages?access_token" . $this->token;    
 
-            $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_HEADER, false);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_HTTPHEADER,
-                    array("Content-type: application/json"));
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: application/json' )); 
+        
+            $json_response = curl_exec($ch);
 
-            $json_response = curl_exec($curl);
-
-            $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
             if ( $status != 201 ) {
-                die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+                die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($ch) . ", curl_errno " . curl_errno($ch));
             }
 
 
-            curl_close($curl);
+            curl_close($ch);
 
             $response = json_decode($json_response, true);
             var_dump($response);
