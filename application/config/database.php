2ff9771552277ccede9c1fd2f889f8fb85465d3b
Eptick <redzicleon@gmail.com>
2017-04-11 21:04:03 +0200

    dev

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,12 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'hostname' => 'ec2-107-22-173-160.compute-1.amazonaws.com',
+	'port'     => '5432',
+	'username' => 'hxorxngptqnhkw',
+	'password' => 'cfdddbe3277896aaf26fc25016bb12589336596b9c90686414ea4d298bcaebcc',
+	'database' => 'd3egl9a6ps9h94',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
--- application/libraries/SendAPI.php
+++ application/libraries/SendAPI.php
@@ -1,22 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Wesly {
+class SendAPI {
         private $token = "EAAKtAJ0lxooBAEXjEXMsAwpPTiZAuBT1gIY97kEEmUeZBHRedJl8CHT8zLaeZA4s0vAeCZAkihPNYsx2R0FSr4swj1Xaxbh3zUDENyRF8a9Si8FeajzorUOvTvHGfN72Q0fUfU0QhF77JZBoYeKqG6BzvoeA67MbdUsgTIrb2OwZDZD";
         public function __construct()
         {
                 // Assign the CodeIgniter super-object
                 $this->CI =& get_instance();
-                $this->CI->load->library('jsonMessages');
-                $this->CI->load->library('sendAPI');
         }
 
-        public function reciveTextMessage($sender, $poruka)
+        public function sendFacebook($json)
         {
-            $json = $this->CI->jsonmessages->createTextMessage($sender, $poruka);
-
-            $this->CI->sendapi->sendFacebook($json);
+            $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$this->token;
             
-
+            $ch = curl_init($url);
+            
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            $result = curl_exec($ch);
         }
 }
