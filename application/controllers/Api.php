cd1ad7f0d91c7343f85cac27986f8fc25df089dd
Eptick <redzicleon@gmail.com>
2017-04-12 20:49:23 +0200

    dodan API za dohvaćanje termin

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Api extends CI_Controller {
     
     public function __construct()
     {
@@ -17,6 +17,23 @@ class Dashboard extends CI_Controller {
 
 			$id = $this->ion_auth->user()->row()->id;
             
-		};
+            $response = array(
+                array("id"    => "1",
+                      "title" => "Naslov 1",
+                      "start" => "2017-04-12"),
+
+                array("id"    => "2",
+                      "title" => "Naslov 2",
+                      "start" => "2017-04-08T10:00:00")
+            );
+            header('Content-Type: application/json');
+            echo json_encode( $response );
+
+		} 
+            else 
+        {
+            header('Content-Type: application/json');
+            echo json_encode( "{}");
+        }
 	}
 }
