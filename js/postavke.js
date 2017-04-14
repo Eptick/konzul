37b62d12d6e1a0538b1929b2c7250cdfe2bb15d5
Eptick <redzicleon@gmail.com>
2017-04-14 16:46:00 +0200

    dev

--- js/postavke.js
+++ js/postavke.js
@@ -1,55 +1,49 @@
-function init_sliders_datum()
-{
-    if( typeof ($.fn.ionRangeSlider) === 'undefined'){ return; }
-		console.log('init_IonRangeSlider');
-        moment.locale("hr");
+function init_sliders_datum() {
+    if (typeof($.fn.ionRangeSlider) === 'undefined') { return; }
+    console.log('init_IonRangeSlider');
+    moment.locale("hr");
     $(".vrijeme_od_do").ionRangeSlider({
         hide_min_max: true,
         min: 0,
-        max: 24*60,
-        from: 60*8,
-        to: 17*60,
+        max: 24 * 60,
+        from: 60 * 8,
+        to: 17 * 60,
         type: 'double',
         step: 5,
         grid: true,
-		prettify: function(num) {
-            var sati = Math.round(num/60);
+        prettify: function(num) {
+            var sati = Math.round(num / 60);
             var string_sati = String(sati);
 
-            var minuta = Math.round(num%60);
+            var minuta = Math.round(num % 60);
             var string_minuta = String(minuta);
-            
+
             console.log(string_sati.length);
 
-            if(string_sati.length == 1)
-            {
+            if (string_sati.length == 1) {
                 string_sati = "0" + string_sati;
             }
-            if(string_minuta.length == 1)
-            {
+            if (string_minuta.length == 1) {
                 string_minuta = "0" + string_minuta;
             }
-         return string_sati + ":" + string_minuta;
-    }
+            return string_sati + ":" + string_minuta;
+        }
     });
-    
+
 
 };
-function init_prikazivanje_slidera()
-{
-    $(".js-switch").click( function () {
-        if( $(this).is(":checked")) 
-        {
-            $("#range"+$(this).attr("id")).parent().addClass("showRange");
-        } 
-         else 
-        {
-            $("#range"+$(this).attr("id")).parent().removeClass("showRange");
+
+function init_prikazivanje_slidera() {
+    $(".js-switch").click(function() {
+        if ($(this).is(":checked")) {
+            $("#range" + $(this).attr("id")).parent().addClass("showRange");
+        } else {
+            $("#range" + $(this).attr("id")).parent().removeClass("showRange");
         }
     });
 }
 
 $(document).ready(function() {
-   init_sliders_datum();
-   init_prikazivanje_slidera();
-});	
+    init_sliders_datum();
+    init_prikazivanje_slidera();
+});
