8678f1970d976adfaacf076a895698edbb63ddd0
Eptick <redzicleon@gmail.com>
2017-04-21 19:34:09 +0200

    Dodan SendGrid

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+$config['protocol'] = 'sendmail';
+$config['mailpath'] = 'C:\xampp\sendmail';
+$config['charset'] = 'iso-8859-1';
+$config['wordwrap'] = TRUE;
+
+
+?>
---
+++ application/libraries/Mailovi.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SendAPI {
+        public function __construct()
+        {
+                // Assign the CodeIgniter super-object
+                $this->CI =& get_instance();
+                
+        }
+
+}
